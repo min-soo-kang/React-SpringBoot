4b8a90406302d286168199a8f4d45d03620390a6
kangminsoo <kms77663284@gmail.com>
2021-04-09 19:41:51 +0900

    login Page

--- frontend/re-app/src/App.js
+++ frontend/re-app/src/App.js
@@ -1,17 +1,11 @@
-import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import React from 'react';
+import SignInSide from './Component/login.js';
 
-import TopMenuComponent from "./Component/TopMenuComponent";
-
-function App() {
-  return (
-    <div className="App">
-      <TopMenuComponent/>
-        안녕하세요!!!!
-    </div>
-  );
-    
+export default class App extends React.Component {
+    render() {
+        return (
+            
+            <SignInSide />
+        )
+    }
 }
-
-export default App;
